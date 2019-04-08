334c40207d17b587b2585924c431125822d9b36a
dudasenna <39406516+dudasenna@users.noreply.github.com>
2019-04-08 13:46:21 -0300

    Update Deslocamento.sv

--- Deslocamento.sv
+++ Deslocamento.sv
@@ -1,9 +1,9 @@
 module Deslocamento (	
-					input logic [1:0] Shift,
-					input logic signed [63:0] Entrada,
-					input logic [5:0] N,
-					output logic [63:0]Saida
-					);
+	input logic [1:0] Shift,
+	input logic signed [63:0] Entrada,
+	input logic [5:0] N,
+	output logic [63:0]Saida
+);
 
 
 always_comb
