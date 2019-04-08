3c31b4d4ce76fcbcabb43e90ac270493f5f2ae5e
Marcelo Francisco Cavalcanti Veloso <48591216+mfcv@users.noreply.github.com>
2019-04-08 14:46:30 -0300

    Update uc.sv

--- uc.sv
+++ uc.sv
@@ -93,8 +93,8 @@ module uc(
 						ALU_SELECTOR = 1; //SOMA 001
 						MEM32_WIRE = 0;
 						IR_WIRE = 0;
-						LOAD_A = 0; //?
-						LOAD_B = 0; //?
+						LOAD_A = 1; //?
+						LOAD_B = 1; //?
 						PROX_ESTADO = BUSCA;
 					end
 					0100000: //SUB
@@ -106,8 +106,8 @@ module uc(
 						ALU_SELECTOR = 2; //SUB 010
 						MEM32_WIRE = 0;
 						IR_WIRE = 0;
-						LOAD_A = 0; //?
-						LOAD_B = 0; //?
+						LOAD_A = 1; //?
+						LOAD_B = 1; //?
 						PROX_ESTADO = BUSCA;
 					end
 				end*/
