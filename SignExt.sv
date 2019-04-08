3e70aefa0c1ff480ac437d704e953d933a0bad6f
Petter <petterm0023@gmail.com>
2019-04-08 15:52:52 -0300

    Add files via upload

--- SignExt.sv
+++ SignExt.sv
@@ -1,10 +1,27 @@
 module SignExt(
 		input logic [31:0]entrada,
+		input logic [6:0] IR6_0,
 		output logic [63:0]saida
 		);
 	always_comb
 	begin 
-		saida[63:12] = 0;
-		saida[11:0] = entrada[31:20];	
+	case(IR6_0)
+		0010011://ADDI
+		begin
+		end
+		1100011://BEQ
+		begin
+		end
+		1100111://BNE
+		begin
+		end
+		0110111://LUI
+		begin
+		end
+	endcase
 	end
 endmodule
+		/*
+		saida[63:12] = 0;
+		saida[11:0] = entrada[31:20];	
+		*/
