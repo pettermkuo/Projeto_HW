daff9cf322675c755d25ba7d9c547c027a2c0ad0
dudasenna <39406516+dudasenna@users.noreply.github.com>
2019-04-16 19:51:18 -0300

    Update up.sv

--- up.sv
+++ up.sv
@@ -8,7 +8,6 @@ wire LOAD_A;//ok
 wire LOAD_B;//ok
 wire PC_WRITE;//ok
 wire MEM32_WIRE;//ok
-wire MEM64_WIRE;
 wire BANCO_WIRE;//ok
 wire LOAD_MDR;//ok
 wire LOAD_ALU_OUT;//ok
@@ -26,6 +25,7 @@ wire [6:0] IR6_0;//ok
 wire [15:0] SAIDA_ESTADO;//ok
 wire [31:0] IR31_0;//ok
 wire [31:0] MEM_TO_IR_32;//ok
+wire [31:25]FUNCT7;
 wire [63:0] MEM_TO_REG;//ok
 wire [63:0] ALUOUT_TO_MUX;
 wire [63:0] REGMEM_TO_MUX;
@@ -44,26 +44,29 @@ wire [63:0] SIGN_OUT;//ok
 wire [63:0] SHIFT_OUT;//ok
 
 uc UC(
- .CLK(CLK),
- .RESET(RESET),
- .ALU_SRCA(ALU_SRCA),
+ .CLK(CLK),//OK
+ .RESET(RESET),//OK
+ .ALU_SRCA(ALU_SRCA),//OK
  .ALU_SRCB(ALU_SRCB),
- .RESET_WIRE(RESET_WIRE),
+ .RESET_WIRE(RESET_WIRE),//OK
  .ALU_SELECTOR(ALU_SELECTOR),
- .PC_WRITE(PC_WRITE),
- .MEM32_WIRE(MEM32_WIRE),
- .MEM64_WIRE(MEM64_WIRE),
- .IR_WIRE(IR_WIRE),
- .IR6_0(IR6_0),
- .IR11_7(IR11_7),
- .IR19_15(IR19_15),
- .IR24_20(IR24_20),
- .LOAD_A(LOAD_A),
- .LOAD_B(LOAD_B),
- .BANCO_WIRE(BANCO_WIRE),
+ .PC_WRITE(PC_WRITE),//OK
+ .MEM32_WIRE(MEM32_WIRE),//OK
+ .DMEM_RW(DMEM_RW),//OK
+ .IR_WIRE(IR_WIRE),//OK
+ .IR6_0(IR6_0),//OK
+ .IR11_7(IR11_7),//OK
+ .IR19_15(IR19_15),//OK
+ .IR24_20(IR24_20),//OK
+ .IR31_0(IR31_0),//OK
+ .LOAD_A(LOAD_A),//OK
+ .LOAD_B(LOAD_B),//OK
+ .BANCO_WIRE(BANCO_WIRE),//OK
+ .LOAD_MDR(LOAD_MDR),//OK
  .Igual(Igual),
  .SAIDA_ESTADO(SAIDA_ESTADO),
- .LOAD_ALU_OUT(LOAD_ALU_OUT)
+ .LOAD_ALU_OUT(LOAD_ALU_OUT),//OK
+ .FUNCT7(FUNCT7)
  );
 mux2 MUX_A( //ok
  .SELETOR(ALU_SRCA),
@@ -120,16 +123,16 @@ bancoReg BANCOREG(//ok
         .dataout2(RS2)
 );
 Memoria32 MEMORIA32(//p
- .raddress(PC_OUT[31:0]), //endereço de ler
- .waddress(), //endereço de esrever (mem32 nunca escreve)
+ .raddress(PC_OUT[31:0]), //endereÃ§o de ler
+ .waddress(), //endereÃ§o de esrever (mem32 nunca escreve)
  .Clk(CLK),
  .Datain(),
  .Dataout(MEM_TO_IR_32),
- .Wr(MEM32_WIRE) //define se é leitura ou escrita
+ .Wr(MEM32_WIRE) //define se Ã© leitura ou escrita
  );
 Memoria64 MEMORIA64(//p
- .raddress(ALU_OUT_TO_MEM), //endereço de ler //PC_IN pq eh a saida do alu_out
- .waddress(ALU_OUT_TO_MEM), //endereço de escrever
+ .raddress(ALU_OUT_TO_MEM), //endereÃ§o de ler //PC_IN pq eh a saida do alu_out
+ .waddress(ALU_OUT_TO_MEM), //endereÃ§o de escrever
  .Clk(CLK),
  .Datain(REG_B_MUX),
  .Dataout(MEM_TO_REG), //saida vai ser a entrada do memory data reg
@@ -173,4 +176,3 @@ register ALU_OUT(//ok
  .DadoIn(PC_IN),
  .DadoOut(ALU_OUT_TO_MEM)
  );
-endmodule
